5c179d1bf2e2ec344f29706753168f82430554b3
shubhamjadhav3112 <shubhamjadhav.sj11@gmail.com>
2023-10-25 11:37:20 +0530

    this is git hub project

---
+++ Gitproject1/src/main/java/com/example/demo/Gitproject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gitproject1Application.class, args);
+	}
+
+}
